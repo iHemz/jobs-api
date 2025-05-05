4ffe566b40c788b1e5a3b50c40b1eb6134e8abc5
Williams Balogun <williams@dexla.ai>
2025-05-05 20:34:38 +0100

    swagger docs added

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
 const { rateLimit } = require("express-rate-limit");
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 const connectDB = require("./db/connect");
 
@@ -34,6 +37,9 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.use(express.static("./public"));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use("/api/v1/auth", authRouter);
